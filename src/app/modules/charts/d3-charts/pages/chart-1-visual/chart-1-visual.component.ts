15fc3b7aa46308c2d1c1f7ec92aee081c7cad922
Pablo Torres <a01630637@itesm.mx>
2024-04-03 12:52:36 -0600

    Feature: se agregan ejemplos de métodos JS y ejemplo de gráfica con D3.js

--- src/app/modules/charts/d3-charts/pages/chart-1-visual/chart-1-visual.component.ts
+++ src/app/modules/charts/d3-charts/pages/chart-1-visual/chart-1-visual.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-chart-1-visual',
   templateUrl: './chart-1-visual.component.html',
   styleUrl: './chart-1-visual.component.scss'
 })
-export class Chart1VisualComponent {
+export class Chart1VisualComponent implements OnInit {
 
+  data = [125, 100, 50, 75, 200];
+  width = 30;
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+  }
 }
